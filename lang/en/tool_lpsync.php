2d3172a5d18be9ed61b0ef69bcaf30c8ed1c1ca9
Ian Wild <ian.wild@skillsforhealth.org.uk>
2017-03-24 09:14:50 +0000

    First commit

---
+++ lang/en/tool_lpsync.php
@@ -0,0 +1,52 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'tool_lpsync', language 'en'
+ *
+ * @package    tool_lpsync
+ * @copyright  2017 Ian Wild
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Competency framework sync';
+$string['importfile'] = 'CSV framework description file';
+$string['invalidimportfile'] = 'File format is invalid.';
+$string['noframeworks'] = 'No competency frameworks have been created yet';
+$string['import'] = 'Import';
+
+$string['parentidnumber'] = 'Parent id number';
+$string['idnumber'] = 'Id number';
+$string['shortname'] = 'Shortname';
+$string['description'] = 'Description';
+$string['descriptionformat'] = 'Description format';
+$string['scalevalues'] = 'Scale values';
+$string['scaleconfiguration'] = 'Scale configuration';
+$string['ruletype'] = 'Rule type (optional)';
+$string['ruleoutcome'] = 'Rule outcome (optional)';
+$string['ruleconfig'] = 'Rule config (optional)';
+$string['isframework'] = 'Is framework';
+$string['taxonomy'] = 'Taxonomy';
+$string['competencyscale'] = 'Competency Scale: {$a}';
+$string['exportid'] = 'Exported id (optional)';
+$string['competencyscaledescription'] = 'Competency scale created by import';
+$string['relatedidnumbers'] = 'Cross referenced competency id numbers';
+$string['confirm'] = 'Confirm';
+$string['confirmcolumnmappings'] = 'Confirm the columns mappings';
+$string['csvdelimiter'] = 'CSV delimiter';
+$string['csvdelimiter_help'] = 'CSV delimiter of the CSV file.';
+$string['encoding'] = 'Encoding';
+$string['encoding_help'] = 'Encoding of the CSV file.';
