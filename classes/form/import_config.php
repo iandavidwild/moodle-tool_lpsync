da9796e3a5c4c80bd770fc88b886fdabc5097f4b
Ian Wild <ian.wild@skillsforhealth.org.uk>
2017-03-26 12:25:41 +0100

    Ensure that framework is imported once it is syncronised.

--- classes/form/import_config.php
+++ classes/form/import_config.php
@@ -69,22 +69,20 @@ class import_config extends moodleform {
         $mform->setDefault('host', $config['host']);
         
         $mform->addElement('text', 'user', get_string('db_user', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('user', PARAM_ALPHANUM);
-        $mform->addRule('user', null, 'required');
+        $mform->setType('user', PARAM_NOTAGS);
         $mform->setDefault('user', $config['user']);
         
         $mform->addElement('password', 'pass', get_string('db_pass', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('pass', PARAM_RAW);
-        $mform->addRule('pass', null, 'required');
+        $mform->setType('pass', PARAM_NOTAGS);
         $mform->setDefault('pass', $config['pass']);
         
         $mform->addElement('text', 'name', get_string('db_name', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('name', PARAM_ALPHANUM);
+        $mform->setType('name', PARAM_NOTAGS);
         $mform->addRule('name', null, 'required');
         $mform->setDefault('name', $config['name']);
         
         $mform->addElement('text', 'table', get_string('db_table', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('table', PARAM_ALPHANUM);
+        $mform->setType('table', PARAM_NOTAGS);
         $mform->addRule('table', null, 'required');
         $mform->setDefault('table', $config['table']);
         
@@ -92,73 +90,72 @@ class import_config extends moodleform {
         $mform->addElement('header', 'mappings', get_string('mappings_header', 'tool_lpsync'));
         
         $mform->addElement('text', 'parentidnumber', get_string('parentidnumber', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('parentidnumber', PARAM_ALPHANUM);
+        $mform->setType('parentidnumber', PARAM_NOTAGS);
         $mform->addRule('parentidnumber', null, 'required');
         $mform->setDefault('parentidnumber', $config['parentidnumber']);
         $mform->addHelpButton('parentidnumber', 'parentidnumber', 'tool_lpsync');
         
         $mform->addElement('text', 'idnumber', get_string('idnumber', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('idnumber', PARAM_ALPHANUM);
+        $mform->setType('idnumber', PARAM_NOTAGS);
         $mform->addRule('idnumber', null, 'required');
         $mform->setDefault('idnumber', $config['idnumber']);
         $mform->addHelpButton('idnumber', 'idnumber', 'tool_lpsync');
         
         $mform->addElement('text', 'shortname', get_string('shortname', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('shortname', PARAM_ALPHANUM);
+        $mform->setType('shortname', PARAM_NOTAGS);
         $mform->addRule('shortname', null, 'required');
         $mform->setDefault('shortname', $config['shortname']);
         $mform->addHelpButton('shortname', 'shortname', 'tool_lpsync');
         
         $mform->addElement('text', 'description', get_string('description', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('description', PARAM_ALPHANUM);
+        $mform->setType('description', PARAM_NOTAGS);
         $mform->addRule('description', null, 'required');
         $mform->setDefault('description', $config['description']);
         $mform->addHelpButton('description', 'description', 'tool_lpsync');
         
         $mform->addElement('text', 'descriptionformat', get_string('descriptionformat', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('descriptionformat', PARAM_ALPHANUM);
+        $mform->setType('descriptionformat', PARAM_NOTAGS);
         $mform->addRule('descriptionformat', null, 'required');
         $mform->setDefault('descriptionformat', $config['descriptionformat']);
         $mform->addHelpButton('descriptionformat', 'descriptionformat', 'tool_lpsync');
         
         $mform->addElement('text', 'scalevalues', get_string('scalevalues', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('scalevalues', PARAM_ALPHANUM);
+        $mform->setType('scalevalues', PARAM_NOTAGS);
         $mform->addRule('scalevalues', null, 'required');
         $mform->setDefault('scalevalues', $config['scalevalues']);
         $mform->addHelpButton('scalevalues', 'scalevalues', 'tool_lpsync');
         
         $mform->addElement('text', 'scaleconfiguration', get_string('scaleconfiguration', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('scaleconfiguration', PARAM_ALPHANUM);
+        $mform->setType('scaleconfiguration', PARAM_NOTAGS);
         $mform->addRule('scaleconfiguration', null, 'required');
         $mform->setDefault('scaleconfiguration', $config['scaleconfiguration']);
         $mform->addHelpButton('scaleconfiguration', 'scaleconfiguration', 'tool_lpsync');
         
         $mform->addElement('text', 'ruletype', get_string('ruletype', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('ruletype', PARAM_ALPHANUM);
+        $mform->setType('ruletype', PARAM_NOTAGS);
         $mform->setDefault('ruletype', $config['ruletype']);
 
         $mform->addElement('text', 'ruleoutcome', get_string('ruleoutcome', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('ruleoutcome', PARAM_ALPHANUM);
+        $mform->setType('ruleoutcome', PARAM_NOTAGS);
         $mform->setDefault('ruleoutcome', $config['ruleoutcome']);
 
         $mform->addElement('text', 'ruleconfig', get_string('ruleconfig', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('ruleconfig', PARAM_ALPHANUM);
+        $mform->setType('ruleconfig', PARAM_NOTAGS);
         $mform->setDefault('ruleconfig', $config['ruleconfig']);
         
         $mform->addElement('text', 'relatedidnumbers', get_string('relatedidnumbers', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('relatedidnumbers', PARAM_ALPHANUM);
-        $mform->addRule('relatedidnumbers', null, 'required');
+        $mform->setType('relatedidnumbers', PARAM_NOTAGS);
         $mform->setDefault('relatedidnumbers', $config['relatedidnumbers']);
         $mform->addHelpButton('relatedidnumbers', 'relatedidnumbers', 'tool_lpsync');
         
         $mform->addElement('text', 'isframework', get_string('isframework', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('isframework', PARAM_ALPHANUM);
+        $mform->setType('isframework', PARAM_NOTAGS);
         $mform->addRule('isframework', null, 'required');
         $mform->setDefault('isframework', $config['isframework']);
         $mform->addHelpButton('isframework', 'isframework', 'tool_lpsync');
         
         $mform->addElement('text', 'taxonomy', get_string('taxonomy', 'tool_lpsync'), array('size'=>'48'));
-        $mform->setType('taxonomy', PARAM_ALPHANUM);
+        $mform->setType('taxonomy', PARAM_NOTAGS);
         $mform->addRule('taxonomy', null, 'required');
         $mform->setDefault('taxonomy', $config['taxonomy']);
         $mform->addHelpButton('taxonomy', 'taxonomy', 'tool_lpsync');
